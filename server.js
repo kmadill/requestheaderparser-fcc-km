272a339ee0574e2f1c0b75499c3c759d5bd48886
Kevin Madill <kmadill@gmail.com>
2017-06-11 15:36:21 +0200

    sixth commit

--- server.js
+++ server.js
@@ -1,16 +1,12 @@
 var express = require("express");
 var path = require("path");
 var http = require("http");
-var useragent = require("express-useragent");
 var app = express();
-
-app.use(useragent.express());
 
 app.get("/", function(req, res) {
   res.setHeader('Content-Type', 'application/json');
   res.send(JSON.stringify({
-    "ipaddress": req.connection.remoteAddress,
-    "ipaddress2": req.headers["x-forwarded-for"],
+    "ipaddress": req.headers["x-forwarded-for"],
     "language": req.headers['accept-language'].split(',')[0],
     "os": req.headers["user-agent"].split(/[()]/)[1]
   }));
