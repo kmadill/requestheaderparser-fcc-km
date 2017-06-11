7acf4c123f7df2b3ba8a80f25cf72fc81fecc307
Kevin Madill <kmadill@gmail.com>
2017-06-11 15:18:40 +0200

    fourth commit

--- server.js
+++ server.js
@@ -1,15 +1,17 @@
 var express = require("express");
 var path = require("path");
 var http = require("http");
-
+var useragent = require("express-useragent");
 var app = express();
+
+app.use(useragent.express());
 
 app.get("/", function(req, res) {
   res.setHeader('Content-Type', 'application/json');
   res.send(JSON.stringify({
-    "ipaddress": req.ip,
-    "language": req.headers['accept-language'],
-    "software": req.headers['user-agent']
+    "ipaddress": req.connection.remoteAddress,
+    "language": req.headers['accept-language'].split(',')[0],
+    "software": req.useragent.os
   }));
 });
 
